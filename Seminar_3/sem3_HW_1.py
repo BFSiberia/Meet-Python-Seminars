34549032916d3cc09d79ee8c47b0c3931e24f63d
Alex <a.v.tarasov@gmail.com>
2022-07-21 20:58:44 +0800

    amend sem3_HW_1 and sem3_HW_2

--- Seminar_3/sem3_HW_1.py
+++ Seminar_3/sem3_HW_1.py
@@ -1,15 +1,18 @@
-# Задайте список из нескольких чисел. Напишите программу, которая найдёт сумму элементов списка, стоящих на нечётной позиции.
+# Задайте список из нескольких чисел. Напишите программу, которая найдёт сумму элементов списка,
+# стоящих на нечётной позиции.
 # Пример: [2, 3, 5, 9, 3] -> на нечётных позициях элементы 3 и 9, ответ: 12
+
 import random
+n= abs(int(input('Введите количество элементов списка: ')))
+l1 = [random.randint(1, 10) for i in range(n)]
 
-list = []
+total = sum(map(lambda x: int(x[1]), filter(
+    lambda i:  i[0] % 2, enumerate(l1))))
 
-for i in range(6):
-    list.append(random.randint(1,10))
+# Версия до знакомства с лекцией №3
+# total =0
+# for i, value in enumerate(l1): # суммирует значение (value) в кортеже
+#     if i%2 !=0:
+#         total+=value
 
-result =0
-for i, value in enumerate(list):
-    if i%2 !=0:
-        result+=value
-
-print('Сумма элементов списка {} на нечетных позициях = {}'.format(list,result))
+print('Сумма элементов списка {} на нечетных позициях = {}'.format(l1, total))
--- Seminar_3/sem3_HW_2.py
+++ Seminar_3/sem3_HW_2.py
@@ -2,10 +2,17 @@
 # Пример: - [2, 3, 4, 5, 6] => [12, 15, 16]
 #         - [2, 3, 5, 6] => [12, 15]
 
+from audioop import reverse
 import random
 
 n = abs(int(input('Задайте количество элементов списка: ')))
-list = [random.randint(1,10) for i in range(n)]
-print(list)
+l_base = [random.randint(1,21) for i in range(n)]
 
-print([list[i]*list[n-1-i] for i in range(int(len(list)/2+0.5))])
+t = map(lambda i: [l_base[i],l_base[n-1-i]], range(int(len(l_base)/2+0.5)))
+
+l_mult =list(map(lambda i:i[0]*i[1], t))
+
+print(f'Произведения пар чисел списка {l_base} => {l_mult}')
+
+# Первый вариант
+# l_mult=[l_base[i]*l_base[n-1-i] for i in range(int(len(l_base)/2+0.5))]
