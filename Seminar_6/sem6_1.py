13a83ba75f52bf3a3d96877cd0f86c41031491e7
Alex <a.v.tarasov@gmail.com>
2022-07-25 22:49:31 +0800

    final calc sem6_1

--- Seminar_6/sem6_1.py
+++ Seminar_6/sem6_1.py
@@ -9,50 +9,51 @@
     # 1+2*3 => 7;   
     # (1+2)*3 => 9;
 
+
+from ast import While
+
+
 def Breckets(x):
     def Calc(x):
-        result=0
         while "*" in x:
             ind = x.index('*')
-            result=x[ind-1]*x[ind+1]
-            x[ind-1]=result
+            x[ind-1]=x[ind-1]*x[ind+1]
             del x[ind:ind+2]
-
         while "/" in x:
             ind = x.index('/')
-            result=x[ind-1]/x[ind+1]
-            x[ind-1]=int(result)
+            x[ind-1]=x[ind-1]/x[ind+1]
             del x[ind:ind+2]
-
         while "-" in x:
             ind = x.index('-')
-            result=x[ind-1]-x[ind+1]
-            x[ind-1]=result
+            x[ind-1]=x[ind-1]-x[ind+1]
             del x[ind:ind+2]
-
         while '+' in x:
             ind = x.index('+')
-            result=x[ind-1]+x[ind+1]
-            x[ind-1]=result
+            x[ind-1]=x[ind-1]+x[ind+1]
             del x[ind:ind+2]
         return x[0]
+    
+    while ')' in x:
+        bClose=x.index(')')
+        bOpen = len(x[:bClose])-x[bClose::-1].index('(')
+        temp=x[bOpen+1:bClose]
+        while temp.count('*')>0 and temp.count('/')>0:
+            a=temp.index('/')
+            b=temp.index('*')
+            if a<b:
+                temp[a-1]=Calc(temp[a-1:a+2])
+                del temp[a:a+2]
+            else:
+                temp[b-1]=Calc(temp[b-1:b+2]) 
+                del temp[b:b+2]
 
-    while '(' in x:
-        brecketO = x.index('(')
-        brecketC=x.index(')')
-        x[brecketO]=Calc(x[brecketO+1:brecketC])
-        del x[brecketO+1:brecketC+1]
+        x[bOpen]=Calc(temp)
+        del x[bOpen+1:bClose+1]  
     return Calc(x)
 
-s1 = '5*(3-6)/(2+1)'
-list='+,-,*,/,),('
-x=[]
-
-for i in s1:
-    if i not in list:
-        x.append(int(i))
-    else:
-        x.append(i)
+s1 = '5*(3-6*(2+1)/(15-5))'
+y=''.join(map(lambda i: i if i.isdigit() else ' '+i+' ', s1))
+x=[int(i) if i.isdigit() else i for i in y.split()]
 
 print(Breckets(x))
 print(eval(s1))
