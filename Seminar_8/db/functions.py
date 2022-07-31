2d4d1ba7bcbc39312599a89c29790b6f1b23b28e
Alex <a.v.tarasov@gmail.com>
2022-07-31 17:08:35 +0800

    add draft DB

---
+++ Seminar_8/db/functions.py
@@ -0,0 +1,17 @@
+def search():
+    answer=input('Что хоите вывести на экран: 1- весь класс; 2- одного ученика: ')
+    if answer=='1':
+        with open ('student.txt', 'r', encoding='utf-8') as f:
+            result=f.read()
+    else:
+        person=input('Введите ФИО ученика: ')
+        with open ('student.txt', 'r', encoding='utf-8') as f:
+            tmp=f.readlines()
+            for i in tmp:
+                if person in i:
+                    result =i
+                    return result
+
+    return result
+
+print(search())
---
+++ Seminar_8/db/input_data.py
@@ -0,0 +1,14 @@
+def data_in():
+    dictionry=\
+        {
+            'Имя': input('Введите ФИО: '),
+            'Дата рождения': input('Введите дату рождения: '),
+            'Класс': input('Введите класс: '),
+            'Место в классе': input('Введите место в классе: '),
+            'Статус': input('Введите статус ученика: ')
+
+        }
+    return dictionry
+
+with open ('student.txt', 'a', encoding='utf-8') as f:
+    f.write(f'{data_in()}\n')
